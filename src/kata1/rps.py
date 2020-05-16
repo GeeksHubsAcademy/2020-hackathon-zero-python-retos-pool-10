a6cd322d407ee37f46fa19fd5677b9c4e7499221
Andres <acpr87@gmail.com>
2020-05-16 19:49:13 +0200

    Kata 1

--- src/kata1/rps.py
+++ src/kata1/rps.py
@@ -7,17 +7,48 @@ options = ["Piedra", "Papel", "Tijeras"]
 #'Ganaste!'
 #'Perdiste!'
 def quienGana(player, ai):
-    return ""
+    ply = player.capitalize()
+    a = ai.capitalize()
+    print(ply)
+    print(a)
+    if ply == a:
+        return "Empate!"
+
+    elif ply == options[2] and a == options[1]:
+        return 'Ganaste!'
+
+    elif ply == options[1] and a == options[0]:
+        return 'Ganaste!'
+
+    elif ply == options[0] and a == options[2]:
+        return 'Ganaste!'
+    
+    else:
+        return 'Perdiste'
+
 
 # Entry Point
 def Game():
-    #
-    #
+    while True:
+        try:
+            print('Selecciona una de las siguientes opciones: ')
+            print()
+            print('Piedra: 1\nPapel: 2\nTijera: 3')
+            print()
+            player = int(input('Digita el número correspondiente a tu selección: \n')) - 1
+
+            break
+        except Exception as e:
+            print(e)
+            print('Opción invalida, intente nuevamente')
     
-    #
-    #
-    
-    winner = quienGana(player, ai)
+    ai = randint(0,2)
+    print(f'Player selection: {options[player]}\n')
+    print(f'ai selection: {options[ai]}\n')
+    winner = quienGana(options[player], options[ai])
 
     print(winner)
+
+# if __name__ == "__main__":
+#     Game()
 
