7eb467a9ff3d7c2664dcf1970b0deb6651dbbf77
Andres <acpr87@gmail.com>
2020-05-16 19:49:34 +0200

    Kata 3

--- src/kata3/bot.py
+++ src/kata3/bot.py
@@ -12,20 +12,32 @@ logger = logging.getLogger(__name__)
 # Definimos algunas funciones para los comandos. Estos generalmente toman los dos argumentos update y context
 def start(update, context):
     """Envia un mensaje cuando se emita el comando /start."""
+    update.message.reply_text("Hola, Geeks!")
     return ""
 
 def help(update, context):
     """Envia un mensaje cuando se emita el comando /help."""
+    update.message.reply_text("Ayudame!")
     return ""
 
 def mayus(update, context):
-        #
-        return ""
+    result = []
+    for word in context.args:
+        result.append(word.upper())
+
+    final_res = ' '.join(result)
+    update.message.reply_text(final_res)
+    return final_res
 
 def alreves(update, context):
     """Repite el mensaje del usuario."""
-    #
-    return ""
+    result = []
+    user_message = update.message.text
+    user_message = user_message.split(' ')
+    user_message = user_message[1::]
+    final_res = ' '.join(user_message)[::-1]
+    update.message.reply_text(final_res)
+    return final_res
 
 def error(update, context):
     """Envia los errores por consola"""
@@ -34,21 +46,23 @@ def error(update, context):
 def main():
     """Inicio del Bot"""
     #Colocamos el Token creado por FatherBot
-    updater = Updater("", use_context=True)
+    updater = Updater("1266332022:AAEu-KvzfwlSbiiBN0-Jo1tNxkcu5w7xSWA", use_context=True)
 
     # Es el Registro de Comandos a través del dispartcher
-    dp = #
+    dp = []
 
     # Añadimos a la lista de Registro todos los comandos con su función [start - help - mayus]
-    #
-    #
-    #
+    updater.dispatcher.add_handler(CommandHandler('start', start))
+    updater.dispatcher.add_handler(CommandHandler('help', help))
+    updater.dispatcher.add_handler(CommandHandler('mayus', mayus))
+    updater.dispatcher.add_handler(CommandHandler('alreves', alreves))
+    
 
     # Este comando es un Trigger que se lanza cuando no hay comandos [alreves]
-    #
+    # 
     
     # Y este espera al error
-    dp.add_error_handler(error)
+    # dp.add_error_handler(error)
 
     # Lanzamos el Bot
     updater.start_polling()
@@ -56,5 +70,5 @@ def main():
     updater.idle()
 
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
